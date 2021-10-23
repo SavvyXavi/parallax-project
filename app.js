07f5950eefe517e6c8f8e1145e70db9911ffa8f1
Xavi-Dev <xjohndev@outlook.com>
2021-10-23 00:13:10 -0400

    Init Parallax project

--- app.js
+++ app.js
@@ -2,3 +2,6 @@ let stars = document.getElementById('stars');
 let moon = document.getElementById('moon');
 let mountains_behind = document.getElementById('mountains_behind');
 let text = document.getElementById('text');
+let btn = document.getElementById('btn');
+let btn = document.getElementById('btn');
+
